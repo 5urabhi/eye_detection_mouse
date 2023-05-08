cc159d81903ed6b4e59dd2b13d760c94d64fc0c3
5urabhi <104481755+5urabhi@users.noreply.github.com>
2023-05-08 18:26:49 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
 import cv2
 import mediapipe as mp
-cam = cv2. VideoCapture (0)
-face_mesh = mp.solutions.face_mesh.FaceMesh()#refine_landmarks=True)
+import pyautogui
+
+cam = cv2.VideoCapture(0)
+face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+screen_w, screen_h = pyautogui.size()
 while True:
-    success, image = cam.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    output = face_mesh.process(image)
+    _, frame = cam.read()
+    frame = cv2.flip(frame, 1)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = face_mesh.process(rgb_frame)
     landmark_points = output.multi_face_landmarks
-    frame_h,frame_w,_=image.shape
+    frame_h, frame_w, _ = frame.shape
     if landmark_points:
         landmarks = landmark_points[0].landmark
-        for landmark in landmarks:
-            x=int(landmark.x)#*frame_w)
-            y=int(landmark.y)#*frame_h)
-            cv2.circle(image, (x, y), 3, (0, 255, 0))
-            print(landmark_points)
-    cv2. imshow ('Eye Controlled Mouse', image)
+        for id, landmark in enumerate(landmarks[474:478]):
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 0))
+            print(id)
+            if id == 0 :
+                screen_x = screen_w * landmark.x
+                screen_y = screen_h * landmark.y
+                pyautogui.moveTo(screen_x, screen_y)
+        left = [landmarks[145], landmarks[159]]
+        right=[landmarks[475],landmarks[477]]
+        for landmark in left:
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 255))
+        for landmark in right:
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 255))
+        if (left[0].y - left[1].y) < 0.004:
+            print(left[0].y - left[1].y)
+            pyautogui.leftClick()
+            pyautogui.sleep(1)
+        if (right[0].y - right[1].y) < 0.004:
+            print(right[0].y - right[1].y)
+            pyautogui.rightClick()
+            pyautogui.sleep(1)
+        if ((left[0].y - left[1].y) < 0.004) and ((right[0].y - right[1].y) < 0.004):
+            pyautogui.click()
+            pyautogui.sleep(1)
+    cv2.imshow('Eye Controlled Mouse', frame)
+    cv2.waitKey(1)
